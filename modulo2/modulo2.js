4d6cf38a4c8d62ede5cacfcf970878d47a0fb234
Thaysa-Miranda <thaysa.mbmiranda@gmail.com>
2022-01-28 16:52:06 -0300

    criacao dos arquivos modulo2.css e modulo2.js e modificacao do arquivo index.html

---
+++ modulo2/modulo2.js
@@ -0,0 +1,12 @@
+function cliqueAvancar(){
+
+    document.getElementById("gato").src="../imagens/gato-fofo.jpg";
+
+};
+
+function cliqueVoltar(){
+
+    document.getElementById("gato").src="../imagens/gato.jpg";
+
+};
+
